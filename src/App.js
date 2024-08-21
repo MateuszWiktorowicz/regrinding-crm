3219ae98eb943389e4b4d01a8515db6648d43e0b
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-08-21 09:12:41 +0200

    add Offer into App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Offer from './components/Offer/Offer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Offer" element={<Offer />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
