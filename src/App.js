05e17505a4475b8d0118f4c74aa67ce78557e7d5
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-08-28 19:47:42 +0200

    add login form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Offer from './components/Offer/Offer';
+import Login from "./components/Login/Login";
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+      return <Login setToken={setToken} />
+  }
   return (
     <div className="wrapper">
       <h1>Application</h1>
